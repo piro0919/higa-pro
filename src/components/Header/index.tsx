62a643de304e94a429e5f195a24ee1cc1c65e1d9
piro <piro.haniwa@gmail.com>
2023-12-27 06:09:44 +0900

    fix: enable scroll on changed hash

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,51 @@
 import { Jost } from "next/font/google";
 import Link from "next/link";
+import { useMemo } from "react";
+import { scroller } from "react-scroll";
 import Spacer from "react-spacer";
 import styles from "./style.module.scss";
 
 const jost = Jost({ subsets: ["latin"], weight: "700" });
 
 export default function Header(): JSX.Element {
+  const items = useMemo(
+    () =>
+      [
+        {
+          name: "ABOUT",
+          to: "about",
+        },
+        {
+          name: "NEWS",
+          to: "news",
+        },
+        {
+          name: "TALENT",
+          to: "talents",
+        },
+        {
+          name: "CONTACT",
+          to: "contact",
+        },
+      ].map(({ name, to }) => (
+        <li key={to}>
+          <Link
+            className={`${styles.link} ${jost.className}`}
+            href={`/#${to}`}
+            onClick={(): void => {
+              scroller.scrollTo(to, {
+                smooth: false,
+              });
+            }}
+            scroll={false}
+          >
+            {name}
+          </Link>
+        </li>
+      )),
+    []
+  );
+
   return (
     <header className={`${styles.header} pattern-cross-dots-lg`}>
       <Spacer grow={1} />
@@ -16,32 +56,7 @@ export default function Header(): JSX.Element {
               TOP
             </Link>
           </li>
-          <li>
-            <Link className={`${styles.link} ${jost.className}`} href="/#about">
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link className={`${styles.link} ${jost.className}`} href="/#news">
-              NEWS
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`${styles.link} ${jost.className}`}
-              href="/#talents"
-            >
-              TALENT
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`${styles.link} ${jost.className}`}
-              href="/#contact"
-            >
-              CONTACT
-            </Link>
-          </li>
+          {items}
         </ul>
       </nav>
       <Spacer grow={1} />
