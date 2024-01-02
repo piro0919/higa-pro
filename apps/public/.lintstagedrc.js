41616fdaeec46c752c68c91c4ede4699b081ea7d
piro <piro.haniwa@gmail.com>
2024-01-02 13:39:43 +0900

    feat: change to monorepo

--- apps/public/.lintstagedrc.js
+++ apps/public/.lintstagedrc.js
@@ -1,13 +1,14 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `next lint ./apps/public --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
 module.exports = {
   "*": "prettier --ignore-unknown --write",
   "**/*.scss": "stylelint --fix",
-  "*.{ts,tsx}": "bash -c 'npx tsc --noemit'",
+  "*.{ts,tsx}":
+    "bash -c 'npx tsc --noemit --project apps/public/tsconfig.json'",
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
 };
